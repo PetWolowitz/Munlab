2e8b37cb5dfc28f045640f8b0003152cecbde532
PetWolowitz <156096630+PetWolowitz@users.noreply.github.com>
2024-10-31 12:13:07 +0100

    Progetto Munlab Impostato

---
+++ backend/users/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import CustomUser
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("username", "email", "password")
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        user = User.object.create.user(
+            username=validated_data["username"],
+            email=validated_data["email"],
+            password=validated_data["password"]
+        )
+        return user
