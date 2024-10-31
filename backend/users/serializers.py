113ad5dded2ed0b7e8f9df1dded8f3db3744cedf
PetWolowitz <156096630+PetWolowitz@users.noreply.github.com>
2024-10-31 12:48:35 +0100

    Login e Register impostati

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework import serializers
+from rest_framework import serializers # type: ignore
 from .models import CustomUser
 from django.contrib.auth import get_user_model
 
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,33 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import UserSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
 from django.shortcuts import render
 
 # Create your views here.
+
+#view per la registrazione di un utente
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+#view per il login di un utente
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+            }, status=status.HTTP_200_OK)
+        return Response({'error': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
